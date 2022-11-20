e046248f7537378aecb7b160063bd573dc465d09
ВадимКа <v.nalimytch@yandex.ru>
2022-11-20 16:12:40 +0500

    zada4a 12

---
+++ task12/Program.cs
@@ -0,0 +1,16 @@
+﻿// прога кот будет принимать 2 числа и выводить, является ли 2-е число кратным 1-му
+// если 2 не кратно 1, то программа выводит остаток от деления
+
+Console.Clear();
+System.Console.Write("Введите первое число ");
+int digit1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число ");
+int digit2 = Convert.ToInt32(Console.ReadLine());
+
+if  (digit2%digit1==0) {
+    Console.WriteLine("числа кратные");
+}
+else {
+int ostatok = digit2%digit1;
+Console.WriteLine($"остаток {ostatok}");
+}
