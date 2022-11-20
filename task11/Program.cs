566b06276c14a87d14ba934903702a9896872222
ВадимКа <v.nalimytch@yandex.ru>
2022-11-20 15:42:15 +0500

    zada4a 11

---
+++ task11/Program.cs
@@ -0,0 +1,8 @@
+﻿// прога кот выводит случ-е 3хзначное число и удаляет вторую цифру этого числа
+
+Console.Clear();
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"sluchainoe chislo - {number}");
+int digit1 = number/100;
+int digit2 = number%10;
+Console.WriteLine($"число {digit1}{digit2}");
