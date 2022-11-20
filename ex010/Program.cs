16a567aae097777a798d8a56578ee39672032108
ВадимКа <v.nalimytch@yandex.ru>
2022-11-20 18:56:37 +0500

    ДЗ. Задача №10

---
+++ ex010/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+
+
+Console.Clear();
+System.Console.Write("Введите трехзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"число - {number}");
+int digit2 = (number%100)/10;
+Console.WriteLine($"число {digit2}");
+
+
