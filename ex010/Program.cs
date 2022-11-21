505d36e8d083d61fa706370aadad4ce64bc6c772
ВадимКа <v.nalimytch@yandex.ru>
2022-11-21 10:18:31 +0500

    доделал условие Задачи № 10

--- ex010/Program.cs
+++ ex010/Program.cs
@@ -7,8 +7,11 @@
 Console.Clear();
 System.Console.Write("Введите трехзначное число ");
 int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"число - {number}");
-int digit2 = (number%100)/10;
-Console.WriteLine($"число {digit2}");
-
+if (99 < number  && number < 1000)
+    {
+    int digit2 = (number%100)/10;
+    Console.WriteLine($"число по середине {digit2}");
+    }
+    else 
+    Console.WriteLine("число не трехзначное");
 
