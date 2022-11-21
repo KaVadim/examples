4b66dd4dedd58b86395b7ad8f6b40babda2f4c87
ВадимКа <v.nalimytch@yandex.ru>
2022-11-21 10:49:41 +0500

    Задача № 13. 1 версия

---
+++ ex013/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// Console.Clear();
+class Example
+{
+static void Main()
+{
+Console.Write("введите число ");
+string username = Console.ReadLine();
+    if (username[2] == 0)
+    { Console.Write("числа нет"); }    
+    else
+    {
+Console.WriteLine(" 3й символ - '{1}'",
+username.Length, username[2]);}
+}
+}
