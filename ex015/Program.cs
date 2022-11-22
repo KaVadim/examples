94a490819ad55ca42297fe9620e78471c027a047
ВадимКа <v.nalimytch@yandex.ru>
2022-11-22 20:23:27 +0500

    Задача № 15

---
+++ ex015/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+int number = ReadInt("Введите число от 1 до 7: ");
+Console.WriteLine(WorkHoliday(number));
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+string WorkHoliday(int a)
+{
+    if (a > 0 && a < 8)
+    {
+        if (a == 7 || a == 6)
+        {
+            Console.Write("Выходной");
+        }
+        else
+        {
+            Console.Write("Рабочий");
+        }
+    }
+    else
+    {
+        Console.Write("число не верно");
+    }
+    return " ";
+}
