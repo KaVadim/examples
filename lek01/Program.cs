686a266f1200453d8c0f89bd27046e578ad49664
ВадимКа <v.nalimytch@yandex.ru>
2022-11-25 11:37:20 +0500

    пример из лекции №3

---
+++ lek01/Program.cs
@@ -0,0 +1,26 @@
+﻿// в тесте пробелы заменить черточками , к на К, С на с
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля.";
+
+// string s = "qwerty"
+//             012...
+// s[3] // r - будет третьим символом из слова "qwerty"
+Console.Clear();
+
+string Replace(string text, char oldVal, char newVal)
+{
+        string result = String.Empty;
+
+        int length = text.Length;
+        for (int i=0; i<length; i++)
+        {
+            if(text[i] == oldVal) result = result + $"{newVal}";
+            else result = result + $"{text[i]}";
+        }
+        return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
