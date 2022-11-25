29e2f2776483562cb33f73d15a61eb23ac133f64
ВадимКа <v.nalimytch@yandex.ru>
2022-11-25 15:17:12 +0500

    задача из семинара № 18 через if

---
+++ task18/Program.cs
@@ -0,0 +1,27 @@
+﻿// прога, кот по номеру четверти показывает диапазон х и у
+
+
+Console.WriteLine ("Введите номер четверти (1,2,3 или 4): ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x==1 || x==2 || x==3 || x==4)
+{
+if(x == 1)
+{
+    Console.WriteLine ("х и у больше 0");
+}
+else if(x == 2)
+{
+    Console.WriteLine ("х меньше 0, у больше");
+}
+else if(x == 3)
+{
+    Console.WriteLine ("х и у меньше 0");
+}
+else if(x == 4)
+{
+    Console.WriteLine ("х больше 0, у меньше");
+}
+}
+else 
+    {Console.WriteLine ("не верно");}
