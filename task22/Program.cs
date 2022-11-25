0e57c4867cc3406846da02420c6f59a91f320415
ВадимКа <v.nalimytch@yandex.ru>
2022-11-25 20:33:24 +0500

    задача из семинара № 22

---
+++ task22/Program.cs
@@ -0,0 +1,8 @@
+﻿// прога, кот принимает число N и выдает таблицу квадратов чисел от 1 до N
+// 5 - 1?4/9?16/25
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+for(int i = 1; i <= num; i++)
+{
+    Console.WriteLine(Math.Pow(i,2)); // или i*i
+}
