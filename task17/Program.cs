b12fe4b1eb5d22571405d465f8ae54c8be2f23f9
ВадимКа <v.nalimytch@yandex.ru>
2022-11-25 14:54:36 +0500

    задача из семинара № 17

---
+++ task17/Program.cs
@@ -0,0 +1,23 @@
+﻿// прога, кот принимае координаты (х,у) и выдает номер четверти
+
+Console.WriteLine ("Введите Х: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if(x > 0 && y > 0)
+{
+    Console.WriteLine ("1");
+}
+else if(x < 0 && y > 0)
+{
+    Console.WriteLine ("2");
+}
+else if(x < 0 && y < 0)
+{
+    Console.WriteLine ("3");
+}
+else if(x > 0 && y < 0)
+{
+    Console.WriteLine ("4");
+}
