2a3d300786e3b0557367311b220e7e63a9561013
ВадимКа <v.nalimytch@yandex.ru>
2022-11-28 20:47:03 +0500

    задача из семинара № 26. решение через string TryPaste

--- task26/Program.cs
+++ task26/Program.cs
@@ -1,10 +1,23 @@
 ﻿// прога, кот принимает число и выдает кол-во цифр в числе
 // 456 - 3
-Console.Write("Введите число ");
-int A = Convert.ToInt32(Console.ReadLine()); // число введено
-string A1 = A.ToString(); // переведет - конвертирует число. Свойства массива
-Console.Write ($"Цифр " + A1.Length);
 
+// версия 3
+Console.Clear();
+Console.Write("введи число");
+string? A = Console.ReadLine(); // принимает сразу строку
+if (int.TryParse(A, out int num))
+    Console.Write("ЦИфр " + A.Length);
+else
+    Console.Write("Тут не цифры");
+
+// 2 версия
+// Console.Write("Введите число ");
+// int A = Convert.ToInt32(Console.ReadLine()); // число введено
+// string A1 = A.ToString(); // переведет - конвертирует число. Свойства массива
+// Console.Write ($"Цифр " + A1.Length);
+
+
+// 1 версия
 // Console.Write ("Введите число ");
 // int num = Math.Abs(Convert.ToInt32(Console.ReadLine())); //  мат.абс. позволяет вводить отрицательные числа
 // int count =0; // это будет счетчиком при делении числа на 10
