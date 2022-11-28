f6484db0ded92b6e01186191fe4138168190cc86
ВадимКа <v.nalimytch@yandex.ru>
2022-11-28 19:21:02 +0500

    задача из семинара № 24

---
+++ task24/Program.cs
@@ -0,0 +1,11 @@
+﻿// прога, кот принимает число А и выдает сумму от 1 до А
+
+Console.Clear();
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for(int i=1; i<=A; i++)
+{
+    sum= sum + i;
+}
+Console.WriteLine($"Сумма: {sum}");
