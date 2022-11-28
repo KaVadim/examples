be2c8ca733fa8103ea4a6fd89af7657615f5036a
ВадимКа <v.nalimytch@yandex.ru>
2022-11-28 20:33:48 +0500

    задача из семинара № 26. решение через string

--- task26/Program.cs
+++ task26/Program.cs
@@ -1,19 +1,23 @@
 ﻿// прога, кот принимает число и выдает кол-во цифр в числе
 // 456 - 3
+Console.Write("Введите число ");
+int A = Convert.ToInt32(Console.ReadLine()); // число введено
+string A1 = A.ToString(); // переведет - конвертирует число. Свойства массива
+Console.Write ($"Цифр " + A1.Length);
 
-Console.Write ("Введите число ");
-int num = Math.Abs(Convert.ToInt32(Console.ReadLine())); //  мат.абс. позволяет вводить отрицательные числа
-int count =0; // это будет счетчиком при делении числа на 10
-if(num !=0) // != - "не равно"
-{
-    while (num>0)
-    {
-        count++;
-        num=num/10;
-    }
-}
-else 
-{
-    count=1;
-}
-Console.WriteLine($" Количество цифр в числе " +count);
+// Console.Write ("Введите число ");
+// int num = Math.Abs(Convert.ToInt32(Console.ReadLine())); //  мат.абс. позволяет вводить отрицательные числа
+// int count =0; // это будет счетчиком при делении числа на 10
+// if(num !=0) // != - "не равно"
+// {
+//     while (num>0)
+//     {
+//         count++;
+//         num=num/10;
+//     }
+// }
+// else 
+// {
+//     count=1;
+// }
+// Console.WriteLine($" Количество цифр в числе " +count);
