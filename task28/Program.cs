a85c47389c7ae8cdf48deb12600ade6f2713c5d6
ВадимКа <v.nalimytch@yandex.ru>
2022-11-28 19:59:03 +0500

    задача из семинара № 28

---
+++ task28/Program.cs
@@ -0,0 +1,11 @@
+﻿// прога, кот принимает число N и ивыдает произведение чисел от 1 до N
+// факториал
+
+Console.WriteLine("Введите число N  для определения его факториала ");
+int N = Convert.ToInt32(Console.ReadLine());
+int Fakt = 1;
+for (int i=1; i<=N; i++)
+{
+    Fakt=Fakt*i;
+}
+Console.WriteLine($"Факториал равен " + Fakt);
