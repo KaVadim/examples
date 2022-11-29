dafe943b47de505aae35363243d808e9f8d991da
ВадимКа <v.nalimytch@yandex.ru>
2022-11-29 19:02:38 +0500

    ДЗ Задача № 27

---
+++ ex027/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Clear();
+Console.Write("Введите число ");
+int i = Convert.ToInt32(Console.ReadLine());
+int num = 0;
+for (int count=1; i>0; count++)
+    {
+    num = num + i%10;
+        i = i/10;
+    }
+ Console.Write($"Сумма цифр в числе равна - {num}");
