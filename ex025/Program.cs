d4b01db23f18918243ba3b55ec1f10d568860639
ВадимКа <v.nalimytch@yandex.ru>
2022-11-29 17:06:56 +0500

    ДЗ Задача № 25. через цикл

--- ex025/Program.cs
+++ ex025/Program.cs
@@ -11,6 +11,12 @@ Console.Write("введите число А - ");
 int A = Convert.ToInt32 (Console.ReadLine());
 Console.Write("введите число B - ");
 int B = Convert.ToInt32 (Console.ReadLine());
-double stepen = Math.Pow (A, B);
+// double stepen = Math.Pow (A, B);
 
+
+int stepen = 1;
+for (int count = 1; count <= B; count++)
+    {
+        stepen = stepen*A;
+    }
 Console.WriteLine ("А в степени В = " + stepen);
