23cc6945971ec718fa3423b22e3153904a844e88
ВадимКа <v.nalimytch@yandex.ru>
2022-11-30 11:17:21 +0500

    задача из семинара № 32

---
+++ task32/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 32: Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+
+Console.Clear();
+Console.Write("Введите длину массива ");
+int dl = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Это созданный массив из {dl} элементов и он же наоборот ");
+int [] arr = new int [dl];
+for (int i=0; i<arr.Length; i++)
+{
+    arr[i] = new Random().Next(-100,100);
+    Console.Write($" {arr[i]}");
+    Console.WriteLine($" {-arr[i]}");
+}
+//Console.Write($" Это массив наоборот из {dl} элементов {arr[i]=arr[i]*(-1)}");
+
