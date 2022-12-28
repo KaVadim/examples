5afa252f4ffdfe21e117d19ad055d2ef4f0cdc16
ВадимКа <v.nalimytch@yandex.ru>
2022-12-28 11:52:20 +0500

    задача 33. камит. все забыл

---
+++ task33/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 33: Задайте массив. 
+// Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+// Console.Clear();
+// int[] FillArray(int size) // вариант из семинара
+// {
+// int[] arr = new int[size];
+// for(int i = 0; i < arr.Length; i++)
+// {
+// arr[i] = new Random().Next(0,10);
+// }
+// return arr;
+// }
+// Console.Write("Введите длину массива: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число ");
+// int cif = Convert.ToInt32(Console.ReadLine());
+// int[] array = FillArray(num);
+// Console.WriteLine("Ваш массив:"+ string.Join(" ",array));
+// for(int i = 0; i == cif; i++)
+// // if (array[i] == cif)
+// {
+//     Console.WriteLine("da");
+// }
+// else
+// {
+//     Console.WriteLine("no");
+// }
+
+
+// // }
+// // Console.WriteLine("Новый массив:"+ string.Join(" ",array)); //
+// // for(int i = 0; i < array.Length; i++)
+// // {
+// // array[i]=;
+// // }
+// // Console.WriteLine("Новый массив:"+ string.Join(" ",array)); //
